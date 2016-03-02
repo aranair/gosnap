0bc50bee3ad9e88fff57a200d79fa91c53c5c5d8
Boa Ho man <boa.homan@gmail.com>
2016-03-03 00:23:20 +0800

    Working parse, initial commit

---
+++ config/config.go
@@ -0,0 +1,15 @@
+package config
+
+type Config struct {
+	DB  database `toml:"database"`
+	Api api      `toml:"api"`
+}
+
+type database struct {
+	User     string `toml:"user"`
+	Password string `toml:"password"`
+}
+
+type api struct {
+	Prefix string `toml:"prefix"`
+}
---
+++ crawler.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+// Helper function to pull the href attribute from a Token
+func getHref(t html.Token) (ok bool, href string) {
+	for _, a := range t.Attr {
+		if a.Key == "href" {
+			href = a.Val
+			ok = true
+		}
+	}
+
+	return
+}
+
+func parseLink(n *html.Node) (title string, url string) {
+	for _, a := range n.Attr {
+		if a.Key == "href" && strings.Index(a.Val, "showthread") == 0 {
+			if strings.Index(n.FirstChild.Data, "WTSell") == 0 {
+				title = n.FirstChild.Data
+				url = a.Val
+				break
+			}
+		}
+	}
+	return
+}
+
+// Extract all http** links from a given webpage
+func crawl(url string, ch chan string, chFinished chan bool) {
+	resp, err := http.Get(url)
+
+	defer func() {
+		chFinished <- true
+	}()
+
+	if err != nil {
+		fmt.Println("ERROR: Failed to crawl \"" + url + "\"")
+		return
+	}
+
+	root, err := html.Parse(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	var f func(*html.Node)
+	f = func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "a" {
+			title, url := parseLink(n)
+			if len(title) > 0 && len(url) > 0 {
+				fmt.Println(title)
+				fmt.Println(url)
+			}
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			f(c)
+		}
+	}
+
+	f(root)
+}
+
+func main() {
+	foundUrls := make(map[string]bool)
+	seedUrls := os.Args[1:]
+
+	chUrls := make(chan string)
+	chFinished := make(chan bool)
+
+	for _, url := range seedUrls {
+		go crawl(url, chUrls, chFinished)
+	}
+
+	for c := 0; c < len(seedUrls); {
+		select {
+		case url := <-chUrls:
+			foundUrls[url] = true
+		case <-chFinished:
+			c++
+		}
+	}
+
+	fmt.Println("\nFound", len(foundUrls), "unique urls:\n")
+
+	for url, _ := range foundUrls {
+		fmt.Println(" - " + url)
+	}
+
+	close(chUrls)
+}
