621a4a5cd6981ea4d453fc572e67d89a6bb1392b
Boa Ho man <boa.homan@gmail.com>
2016-04-09 20:30:43 +0800

    Add cron functionality

--- app.go
+++ app.go
@@ -4,11 +4,14 @@ import (
 	"log"
 
 	"github.com/aranair/gosnap/config"
+	"github.com/aranair/gosnap/crawler"
+	"github.com/aranair/gosnap/models"
 	"github.com/gin-gonic/gin"
 
 	_ "github.com/lib/pq"
 
 	"github.com/BurntSushi/toml"
+	"github.com/bamzi/jobrunner"
 )
 
 func main() {
@@ -17,10 +20,12 @@ func main() {
 		log.Fatal(err)
 	}
 
+	jobrunner.Start()
+	jobrunner.Schedule("@every 30s", UpdateListings{})
+
 	pqStr := "user=" + conf.DB.User + " password='" + conf.DB.Password + "' dbname=gosnap host=localhost sslmode=disable"
 
 	config.InitDb(pqStr)
-
 	router := gin.Default()
 
 	var api = __api__{}
@@ -28,9 +33,25 @@ func main() {
 	// Attach api
 	api.bind(router.Group(conf.Api.Prefix))
 
-	// For all other requests, see: react.go.
-	// react.bind(router)
-
-	// Start listening
+	router.GET("/jobrunner/json", JobJson)
+	routes.LoadHTMLGlob("../github.com/bamzi/jobrunner/views/Status.html")
+	routes.GET("/jobrunner/html", JobHtml)
 	router.Run(":5000")
+}
+
+type UpdateListings struct{}
+
+func (e UpdateListings) Run() {
+	urls := map[int][]string{
+		1: []string{"http://www.clubsnap.com/forums/forumdisplay.php?f=104"},
+		2: []string{"http://www.clubsnap.com/forums/forumdisplay.php?f=102"},
+		3: []string{"http://www.clubsnap.com/forums/forumdisplay.php?f=111"},
+	}
+
+	for cid, urla := range urls {
+		l := crawler.Start(urla)
+		for title, url := range l {
+			models.CreateListing(cid, title, url)
+		}
+	}
 }
